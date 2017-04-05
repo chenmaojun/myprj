542eb7e7876a7e373acc7d309ca876fd646f0274
Ghostv <Ghostv>
2017-04-05 21:35:29 +0800

    this is my test commitgit add TestPrj!

---
+++ TestPrj/src/com/cmj/Test.java
@@ -0,0 +1,16 @@
+package com.cmj; 
+
+/**
+ * @Description: TODO 
+ * @date 2017年4月5日 下午8:49:12 
+ * @author mjchen
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello word!");
+	}
+
+}
+ 
